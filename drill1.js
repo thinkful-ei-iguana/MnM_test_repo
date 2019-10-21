eb95822575e4e8821e723fa8c3ecb79503921cc9
Mason Reichbauer <mreichba@mail.usf.edu>
2019-10-21 15:52:19 -0400

    try/catch finished

--- drill1.js
+++ drill1.js
@@ -1,19 +1,27 @@
 function getYearOfBirth(age) {
+    try{ 
     const yearOfBirth = 2019 - age;
     if (age < 0) {
         throw new Error("Age can not be negative");
     } else {
     return yearOfBirth;
     }
+    } catch (e){
+        throw new Error(e.message)
+    }
 }
 
 function createGreeting(name, age) {
+    try {
     const yob = getYearOfBirth(age);
     return `Hi, my name is ${name} and I am ${age} years old.
     I was born in ${yob}`;
+    } catch (e) {
+        return e.message;
+    }
 }
-try {
-const greeting1 = createGreeting("Maggie", -30);
-} catch (e){
-   console.log(e.message)
-}
+
+const greeting1 = createGreeting("Maggie", 30);
+console.log(greeting1);
+//let birthYear = getYearOfBirth(30);
+//console.log(birthYear);
