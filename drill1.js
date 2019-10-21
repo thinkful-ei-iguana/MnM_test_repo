ca4c04c48cee6e3badf3b941647329e4d1ef7241
therealmbittarelli <maggiekmcclellan@gmail.com>
2019-10-21 14:46:00 -0400

    created YOB function

--- drill1.js
+++ drill1.js
@@ -1,8 +1,12 @@
-function createGreeting(name, age) {
+function getYearOfBirth(age) {
     const yearOfBirth = 2019 - age;
+    return yearOfBirth;
+}
 
-return `Hi, my name is ${name} and I am ${age} years old.
-I was born in ${yearOfBirth}.`;
+function createGreeting(name, age) {
+    const yob = getYearOfBirth(34);
+    return `Hi, my name is ${name} and I am ${age} years old.
+    I was born in ${yob}`;
 }
 const greeting1 = createGreeting("Maggie", 34);
 console.log(greeting1);
